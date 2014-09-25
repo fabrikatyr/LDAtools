2a37ee57583f5da6031a405781fa5e5b7545719c
fabrikatyr <conor@fabrikatyr.com>
2014-09-25 17:35:11 +0100

    Update Lda.c for Windows
    Getting rid for DRAND48 error
    http://stackoverflow.com/questions/11065861/drand48-on-windows

--- src/lda.c
+++ src/lda.c
@@ -243,7 +243,7 @@ void free_dmatrix(double **v, int nrow){
 void sample(int first, int last, double *prob, int *z){
   int j, l = last - first + 1;
   double x, s;
-  x = drand48();
+  x = (double)rand()/RAND_MAX;
   s = 0.0;
   for (j = 0; j < l; j++){
     s += prob[j];
